ce1bdde61f06393060833d725b9f064e69c1cda9
Yash280160 <44415173+Yash280160@users.noreply.github.com>
2018-10-27 21:53:13 +0530

    Add files via upload
    A program that prints 10001th term of prime number series.

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
 class Main {
   public static void main(String[] args) {
-   
-    long i,a=2520,b=0;
-    do{
-      for (i=1;i<=20;i++)
-      {
-        if (a%i==0)
-        b=b+1;
-      }
-      a++;
-      if (b==20)
+   int i,a,b=1;
+   for (a=3;b<10001;a++)
+   {
+   for (i=2;a>i;i++)
+   { if (a%i==0)
       break;
-    }while(true);
-     System.out.print("The smallest number which is divisible by all numbers from 1 to 20:"+a);
-    }
-
-   
+   }
+   if (a==i)
+   b++;
+   }
+   System.out.print("10001th prime number is:"+(a-1));
   }
+}
