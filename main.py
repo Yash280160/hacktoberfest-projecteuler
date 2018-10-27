06a6b1ac7fc2aa85ea1f43888575105e917486bc
Yash280160 <44415173+Yash280160@users.noreply.github.com>
2018-10-27 22:38:07 +0530

    Add files via upload
    Prints the  largest prime number factor of  600851475143.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-sum = 0
-b= 0
-for i in range(20000001):
-    for j in range(20000001):
+b=0
+for i in range(1,600851475143):
+  if(600851475143 % i == 0):
+    for j in range(1,600851475143):
       if(i%j==0):
-        b = b+ 1
-    if(b==0):
-      sum = sum + i
-print("The sum of numbers:"sum)
+        b = b + 1
+      if(b==0):
+        prime_fact = i
+print(i)
+
+
+
