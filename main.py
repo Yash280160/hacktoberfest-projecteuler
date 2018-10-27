1e842daddd7938b8211e0e531d571ff9f39fcd8d
Yash280160 <44415173+Yash280160@users.noreply.github.com>
2018-10-27 22:16:25 +0530

    Add files via upload
    Sum of all prime numbers that are below 2 million.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 sum = 0
-for i in range(1001):
-  if(i%3==0 & i%5==0):
-    sum = sum + 1
-print(sum)
+b= 0
+for i in range(20000001):
+    for j in range(20000001):
+      if(i%j==0):
+        b = b+ 1
+    if(b==0):
+      sum = sum + i
+print("The sum of numbers:"sum)
